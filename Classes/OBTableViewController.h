277a54b72ffbbf13aea9bc0856da602b04e2ff00
Aaron Griffith <aargri@gmail.com>
2010-03-18 22:26:44 -0400

    added a base class for our custom table cells in the future

---
+++ Classes/OBTableViewController.h
@@ -0,0 +1,16 @@
+// OSU Bus - a client for the OSU Bus System
+// Copyright (C) 2010 Aaron Griffith
+//
+// This file is licensed under the GNU GPL v2. See
+// the file "main.m" for details.
+
+#import <UIKit/UIKit.h>
+
+@interface OBTableViewController : UITableViewController
+{
+	UITableViewCell* newCell;
+}
+
+@property (nonatomic, retain) IBOutlet UITableViewCell* newCell;
+
+@end
---
+++ Classes/OBTableViewController.m
@@ -0,0 +1,13 @@
+// OSU Bus - a client for the OSU Bus System
+// Copyright (C) 2010 Aaron Griffith
+//
+// This file is licensed under the GNU GPL v2. See
+// the file "main.m" for details.
+
+#import "OBTableViewController.h"
+
+@implementation OBTableViewController
+
+@synthesize newCell;
+
+@end
