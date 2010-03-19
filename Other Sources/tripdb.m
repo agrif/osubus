2aff050955ecf6dd606f3a87f34a06892b211483
Aaron Griffith <aargri@gmail.com>
2010-03-18 20:08:05 -0400

    added actual code to database updater

--- Other Sources/tripdb.m
+++ Other Sources/tripdb.m
@@ -1,11 +1,37 @@
-#import <Foundation/Foundation.h>
+// tripdb - a database updater for OSU Bus
+// Copyright (C) 2010 Aaron Griffith
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-int main (int argc, char** argv)
+#import <Foundation/Foundation.h>
+#import "OTClient.h"
+
+int main(int argc, char** argv)
 {
     NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
+	
+	if (argc != 2)
+	{
+		NSLog(@"Usage: tribdb [db path]");
+		return 1;
+	}
+	
+	[[OTClient sharedClient] setAPIKey: @"HgejWEsJAycCRf8gzsSWVHMcy"];
+	[[OTClient sharedClient] setDatabasePath: [NSString stringWithCString: argv[1]]];
+	
+	[[OTClient  sharedClient] updateDatabase];
 	
     [pool drain];
     return 0;
