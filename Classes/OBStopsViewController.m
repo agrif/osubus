892a45f52c77aef81d4ee874c89ba256337307b0
Aaron Griffith <aargri@gmail.com>
2010-04-17 17:40:14 -0400

    added stub for Stops Controller

---
+++ Classes/OBStopsViewController.m
@@ -0,0 +1,65 @@
+// OSU Bus - a client for the OSU Bus System
+// Copyright (C) 2010 Aaron Griffith
+//
+// This file is licensed under the GNU GPL v2. See
+// the file "main.m" for details.
+
+#import "OBStopsViewController.h"
+
+#import "OTClient.h"
+
+@implementation OBStopsViewController
+
+- (void) viewDidLoad
+{
+	[super viewDidLoad];
+	
+	stops = [[OTClient sharedClient] routes];
+	NSLog(@"Loaded OBStopsViewController");
+	
+	[self.navigationItem setTitle: @"Routes"];
+}
+
+- (void) viewDidUnload
+{
+	[super viewDidUnload];
+	[stops release];
+}
+
+#pragma mark Table View Data Source
+
+- (NSInteger) numberOfSectionsInTableView: (UITableView*) tableView;
+{
+	return 1;
+}
+
+- (NSInteger) tableView: (UITableView*) tableView numberOfRowsInSection: (NSInteger) section
+{
+	return [stops count];
+}
+
+- (NSString*) tableView: (UITableView*) tableView titleForHeaderInSection: (NSInteger) section
+{
+	return nil;
+}
+
+- (UITableViewCell*) tableView: (UITableView*) tableView cellForRowAtIndexPath: (NSIndexPath*) indexPath;
+{	
+	return [self routesCellForTable: tableView withData: [stops objectAtIndex: [indexPath row]]];
+}
+
+#pragma mark Table View Delegate
+
+- (NSIndexPath*) tableView: (UITableView*) tableView willSelectRowAtIndexPath: (NSIndexPath*) indexPath
+{
+	return indexPath;
+}
+
+- (void) tableView: (UITableView*) tableView didSelectRowAtIndexPath: (NSIndexPath*) indexPath
+{
+	// navigation logic
+	
+	[tableView deselectRowAtIndexPath: indexPath animated: YES];
+}
+
+@end
