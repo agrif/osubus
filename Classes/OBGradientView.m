c3c72c148e80112ef6304e1222e2b9c068351282
Aaron Griffith <aargri@gmail.com>
2010-12-14 03:38:26 -0500

    clarified that the OBGradientView I use is different from others floating around

--- Classes/OBGradientView.m
+++ Classes/OBGradientView.m
@@ -4,6 +4,11 @@
 // This file is licensed under the GNU GPL v2. See
 // the file "main.m" for details.
 
+// may seem to be related to the OBGradientView at
+// http://oleb.net/blog/2010/04/obgradientview-a-simple-uiview-wrapper-for-cagradientlayer/
+// but this is just a happy coincidence -- OB just happens to be my namespace for this
+// little project, while this other class was written by Ole Begemann *shrug*
+
 #import "OBGradientView.h"
 
 @implementation OBGradientView
