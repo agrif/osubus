38c188f80635185db5faaa4d37989c211b47cdbe
Aaron Griffith <aargri@gmail.com>
2010-12-27 10:04:20 -0500

    fixed view controller unloading, which was previously incorrect

---
+++ Sources/Extra/UIViewController+ViewDidLoadPairing.m
@@ -0,0 +1,17 @@
+// OSU Bus - a client for the OSU Bus System
+// Copyright (C) 2010 Aaron Griffith
+//
+// This file is licensed under the GNU GPL v2. See
+// the file "main.m" for details.
+
+#import "UIViewController+ViewDidLoadPairing.h"
+
+@implementation UIViewController (ViewDidLoadPairing)
+
+- (void) dealloc
+{
+	[self didReceiveMemoryWarning];
+	[super dealloc];
+}
+
+@end
